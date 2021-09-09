8f968e17803d2933d4c982c1a102810d331e0e30
Igorios <borgesigor153@gmail.com>
2021-09-08 22:49:58 -0300

    Adicionando imagens nas cartas

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 (function(){
+    let images = [];
+
+    for(let i = 0; i < 16; i++) {
+        let img = {
+            src: "image/" + i + ".png",
+            id: i % 8
+        };
+        images.push(img);
+    };
+
     startGame();
 
     function startGame() {
+        let frontFaces = document.getElementsByClassName('front');
+
         for(let i = 0; i < 16; i++) { //Todos os cards
             let card = document.querySelector("#card" + i);
             //Distribuição//
@@ -9,14 +21,17 @@
             card.style.top = i < 8 ? 5 + "px" : 250 + "px";
 
             card.addEventListener('click', flipCard, false);
+
+            frontFaces[i].style.background = "url('"+ images[i].src +"')";
+            frontFaces[i].setAttribute('id', images[i].id);
         }
-    }
+    };
 
     function flipCard() {
-        var faces = this.getElementsByClassName('face');
+        let faces = this.getElementsByClassName('face');
         //Criando classes sem o html//
         faces[0].classList.toggle('flipped');
         faces[1].classList.toggle('flipped');
-    }
+    };
 
 }());
